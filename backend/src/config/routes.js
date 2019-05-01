aa9e08c10e117dc45104eaa0df0bc65214faea6e
= <=>
2019-05-01 13:46:14 -0300

    Implementado autenticação no backend

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,14 +1,19 @@
+
 const express = require('express')
+const auth = require('./auth')
+module.exports = function(server) {
+  
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+  protectedApi.use(auth)
+  const BillingCycle = require('../api/billingCycle/billingCycleService')
+  BillingCycle.register(protectedApi, '/billingCycles')
 
-
-module.exports = function(server){
-
-    const router = express.Router()
-
-    server.use('/api',router)
-
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-
-    BillingCycle.register(router,'/billingCycles')
-
+  
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+  const AuthService = require('../api/user/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }
